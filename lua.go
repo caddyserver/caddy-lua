6969318365a1de05cf83adb52e84074e61e7d890
Matt Butcher <mbutcher@engineyard.com>
2015-09-02 15:11:59 -0600

    Separate interpreter from the HTTP handler.

--- lua.go
+++ lua.go
@@ -3,6 +3,8 @@ package lua
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -69,50 +71,113 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) 
 			return http.StatusInternalServerError, err
 		}
 
-		L := lua.NewState()
-		defer L.Close()
-
-		// buf is filled with the output (print) of the Lua script
-		var buf bytes.Buffer
-
-		L.SetGlobal("print", L.NewFunction(func(L *lua.LState) int {
-			top := L.GetTop()
-			for i := 1; i <= top; i++ {
-				buf.WriteString(L.Get(i).String())
-				if i != top {
-					buf.WriteString(" ")
-				}
-			}
-			buf.WriteString("\n")
-			return 0
-		}))
-
-		// Extract all Lua snippets and execute them
-		// TODO: We're starting with just the first one... put this into a loop of some sort
-		startToken, endToken := []byte("<?lua"), []byte("?>")
-		if startPos := bytes.Index(contents, startToken); startPos != -1 {
-			start := startPos + len(startToken)
-			end := bytes.Index(contents[start:], endToken) + start // BUG: For now, I'm assuming "?>" doesn't appear in the Lua code anywhere
-			if end <= start {
-				return http.StatusInternalServerError, errors.New("Un-closed Lua block") // TODO: Better message
-			}
-			luaSnippet := string(contents[start:end])
-
-			if err := L.DoString(luaSnippet); err != nil {
-				return http.StatusInternalServerError, err
-			}
-
-			// Replace the Lua block with its output
-			contents = append(contents[:startPos], append(buf.Bytes(), contents[end+len(endToken):]...)...)
+		var out bytes.Buffer
+		if err := Interpret(&out, contents); err != nil {
+			return http.StatusInternalServerError, err
 		}
 
 		// Write the combined text to the http.ResponseWriter
-		w.Write(contents)
+		w.Write(out.Bytes())
 
 		return http.StatusOK, nil
 	}
 
 	return h.Next.ServeHTTP(w, r)
+}
+
+// Interpret reads a source, executes any Lua, and writes the results.
+//
+// This assumes that the reader has Lua embedded in `<?lua ... ?>` sections.
+func Interpret(out io.Writer, src []byte) error {
+	L := lua.NewState()
+	defer L.Close()
+
+	var luaOut bytes.Buffer
+	var luaIn bytes.Buffer
+
+	// TODO: If a user uses any concurrent processing here, do we
+	// need to add a lock to the buffer?
+	L.SetGlobal("print", L.NewFunction(func(L *lua.LState) int {
+		top := L.GetTop()
+		for i := 1; i <= top; i++ {
+			luaOut.WriteString(L.Get(i).String())
+			if i != top {
+				luaOut.WriteString(" ")
+			}
+		}
+		luaOut.WriteString("\n")
+		return 0
+	}))
+
+	inCode := false
+	line := 1
+	for i := 0; i < len(src); i++ {
+		if src[i] == '\n' {
+			line++
+		}
+		if inCode {
+			if isEnd(i, src) {
+				//fmt.Println("Sending to Lua interpreter:", luaIn.String())
+				i++ // Skip two characters: ? and >
+				if err := L.DoString(luaIn.String()); err != nil {
+					// TODO: Need to make it easy to tell that this is a
+					// parse error.
+					return fmt.Errorf("Lua Error (Line %d): %s", line, err)
+				}
+				out.Write(luaOut.Bytes())
+				luaIn.Reset()
+				luaOut.Reset()
+				inCode = false
+			} else {
+				luaIn.WriteByte(src[i])
+			}
+		} else {
+			if isStart(i, src) {
+				i += 4
+				inCode = true
+			} else if _, err := out.Write([]byte{src[i]}); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Handle the case where a file ends inside of a <?lua block.
+	// Mimic PHP's behavior.
+	if inCode && luaIn.Len() > 0 {
+		fmt.Printf("sending to Lua interpreter: %s", luaIn.String())
+		if err := L.DoString(luaIn.String()); err != nil {
+			// TODO: Need to make it easy to tell that this is a
+			// parse error.
+			return fmt.Errorf("Lua Error (Line %d): %s", line, err)
+		}
+		out.Write(luaOut.Bytes())
+	}
+
+	return nil
+}
+
+var startSeq = []byte{'<', '?', 'l', 'u', 'a'}
+
+func isStart(start int, slice []byte) bool {
+	if start+5 >= len(slice) {
+		return false
+	}
+	for i := 0; i < 5; i++ {
+		if startSeq[i] != slice[start+i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isEnd(start int, slice []byte) bool {
+	if start+1 >= len(slice) {
+		return false
+	}
+	if slice[start] == '?' && slice[start+1] == '>' {
+		return true
+	}
+	return false
 }
 
 func parse(c *setup.Controller) ([]Rule, error) {
